a52f22002b7bd931c8b62e46aea1f847e93916fe
BuiPhuoc <146042853+buiphuocc@users.noreply.github.com>
2024-06-19 09:59:20 +0700

    commit

--- BBMSRazorPages/Pages/Index.cshtml.cs
+++ BBMSRazorPages/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Interfaces;
 
 namespace BBMSRazorPages.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ICourtSlotService courtSlotService;
+        public List<CourtSlot> CourtSlots { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ICourtSlotService courtSlotService)
         {
             _logger = logger;
+            this.courtSlotService = courtSlotService;
         }
 
         public void OnGet()
         {
-
+            CourtSlots = courtSlotService.GetCourtSlots();
         }
     }
 }
---
+++ Services/CourtSlotService.cs
@@ -0,0 +1,39 @@
+﻿using BusinessObjects;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CourtSlotService : ICourtSlotService
+    {
+        private readonly ICourtSlotService courtSlotService;
+
+        public CourtSlotService(ICourtSlotService courtSlotService)
+        {
+            this.courtSlotService = courtSlotService;
+        }
+        public bool CreateCourtSlot(CourtSlot courtSlot)
+        {
+            return courtSlotService.CreateCourtSlot(courtSlot);
+        }
+
+        public CourtSlot? GetCourtSlotById(int id)
+        {
+            return courtSlotService.GetCourtSlotById(id);
+        }
+
+        public List<CourtSlot> GetCourtSlots()
+        {
+            return courtSlotService.GetCourtSlots();
+        }
+
+        public bool UpdateCourtSlot(CourtSlot courtSlot)
+        {
+            return courtSlotService.UpdateCourtSlot(courtSlot);
+        }
+    }
+}
